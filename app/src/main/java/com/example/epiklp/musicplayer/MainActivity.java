9b0529b66909d958770cc0546dc8b7194c575c6e
Rafal <rafalrybowski1996@gmail.com>
2018-03-24 00:12:06 +0100

    add sliders to hz

--- app/src/main/java/com/example/epiklp/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/epiklp/musicplayer/MainActivity.java
@@ -15,15 +15,17 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.Vector;
+
 public class MainActivity extends AppCompatActivity {
 
     //look
     private LinearLayout mLinearLayout;
-    private TextView currentText, maxText;
+    private TextView currentText;
     private ProgressBar progressBar;
     private TextView headText;
-    private TableLayout table;
-    private TableRow[] rows;
+    private int[] time = new int[2];
+    private String timeSong;
 
     //Player
     private MediaPlayer mMediaPlayer;
@@ -33,11 +35,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        time[0]=0;
+        time[1]=0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mine);
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
         mMediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                time[1] = time[0] = 0;
+            }
+        });
         mMediaPlayer.start();
         mMediaPlayer.setLooping(true);
 
@@ -50,11 +60,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateProgressBar()
     {
-        maxText.setText(String.format("%.2f", mMediaPlayer.getDuration() / 60000f));
+        timeSong = String.format("%.2f", mMediaPlayer.getDuration() / 60000f);
         handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
-                currentText.setText(String.format("%.2f", mMediaPlayer.getCurrentPosition() / 60000f) + "/");
+                if(time[1] < 10)
+                    currentText.setText(time[0] + ".0" + time[1]+"/"+timeSong);
+                else
+                    currentText.setText(time[0] + "." + time[1]+"/"+timeSong);
                 progressBar.setProgress(mMediaPlayer.getCurrentPosition());
             }
         };
@@ -67,6 +80,12 @@ public class MainActivity extends AppCompatActivity {
             while (true) {
                 try {
                     Thread.sleep(1000);
+                    time[1]++;
+                    if(time[1] == 60)
+                    {
+                        time[0]++;
+                        time[1]=0;
+                    }
                     handler.sendMessage(handler.obtainMessage());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -77,42 +96,55 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void setupFxAndUI() {
-        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT,1f);
-        TableRow.LayoutParams tableRowParams = new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, 1f);
+
+        //head
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams tableRowParams = new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 1f);
         mLinearLayout = (LinearLayout) findViewById(R.id.LinearLinearLayout);
+
+        TableLayout table;
+        Vector<TableRow> rows = new Vector<>();
+
         table = new TableLayout(this);
         table.setLayoutParams(linearLayoutParams);
-        rows = new TableRow[3];
         headText = new TextView(this);
         headText.setText("Equalizer");
         headText.setTextSize(20);
         headText.setGravity(Gravity.CENTER_HORIZONTAL);
         headText.setLayoutParams(tableRowParams);
-        rows[0] = new TableRow(this);
-        rows[0].addView(headText);
-        rows[0].getChildAt(0);
-        table.addView(rows[0]);
+        rows.add(new TableRow(this));
+        rows.get(0).addView(headText);
+        rows.get(0).getChildAt(0);
+        table.addView(rows.get(0));
+        //Timer
         currentText = new TextView(this);
         currentText.setText("0/");
         currentText.setTextSize(16);
-        currentText.setGravity(Gravity.RIGHT);
+        currentText.setGravity(Gravity.CENTER);
         currentText.setLayoutParams(tableRowParams);
-        maxText = new TextView(this);
-        maxText.setText("9");
-        maxText.setTextSize(16);
-        maxText.setGravity(Gravity.LEFT);
-        maxText.setLayoutParams(tableRowParams);
-        rows[1] = new TableRow(this);
-        rows[1].addView(currentText);
-        rows[1].addView(maxText);
-        table.addView(rows[1]);
+        rows.add(new TableRow(this));
+        rows.get(1).addView(currentText);
+        table.addView(rows.get(1));
+        //progressBar
         progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
         progressBar.setMax(mMediaPlayer.getDuration());
         progressBar.setProgress(0);
         progressBar.setLayoutParams(tableRowParams);
-        rows[2] = new TableRow(this);
-        rows[2].addView(progressBar);
-        table.addView(rows[2]);
+        rows.add(new TableRow(this));
+        rows.get(2).addView(progressBar);
+        table.addView(rows.get(2));
+
+        short numberFrequency = mEqualizer.getNumberOfBands();
+        final short lower = mEqualizer.getBandLevelRange()[0];
+        final short upper = mEqualizer.getBandLevelRange()[1];
+        for(short counter = 0; counter < numberFrequency; ++counter) {
+            TextView frequencyTextView = new TextView(this);
+            frequencyTextView.setLayoutParams(tableRowParams);
+            frequencyTextView.setText(mEqualizer.getCenterFreq(counter)/1000 + "Hz");
+            rows.add(new TableRow(this));
+            rows.lastElement().addView(frequencyTextView);
+            table.addView(rows.lastElement());
+        }
         mLinearLayout.addView(table);
     }
 
